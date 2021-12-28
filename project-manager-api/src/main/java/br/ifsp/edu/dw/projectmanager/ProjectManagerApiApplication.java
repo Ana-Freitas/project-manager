bf4331275731c4d5807be53368a687fce2352f9b
Ana-Freitas <ana_paula.10@hotmail.com>
2021-12-27 23:55:22 -0300

    Init repository

---
+++ project-manager-api/src/main/java/br/ifsp/edu/dw/projectmanager/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.ifsp.edu.dw.projectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerApiApplication.class, args);
+	}
+
+}
---
+++ project-manager-api/src/test/java/br/ifsp/edu/dw/projectmanager/ProjectManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.edu.dw.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
